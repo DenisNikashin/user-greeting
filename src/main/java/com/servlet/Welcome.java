d89f76efb8300e7baa2171e3c4fea39da6c20578
Denis_N <denis.n.82.dn@gmail.com>
2016-11-20 19:57:12 +0200

    2 commit.Adding the local host

--- src/main/java/com/servlet/Welcome.java
+++ src/main/java/com/servlet/Welcome.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/")
 public class Welcome extends HttpServlet{
@@ -14,10 +13,9 @@ public class Welcome extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
                                                     throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-
-        out.println("My Servlet ");
+        String username = req.getParameter("username");
+        resp.getWriter().println("<!DOCTYPE HTML >");
+        resp.getWriter().println("<html><body><p><h1 align=\"center\">" + " Welcome " + username + "</h1></p></body></html>");
     }
 
 }
